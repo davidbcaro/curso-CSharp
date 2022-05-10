effe301f6835dcc3b4908e0eea790e30b66b150c
David Bohorquez <davidbcaro@gmail.com>
2022-05-10 12:00:29 -0500

    Add class and instances

---
+++ 10-Clases_instancias/10-Clases_instancias/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Circulo miCirculo; // Creación del objeto tipo Círuclo
+miCirculo = new Circulo(); // Instanciar una clase. Creación de un ejemplar de la clase
+Console.WriteLine($"El área del circulo es: {miCirculo.calcularArea(6)} cm");
+class Circulo
+{
+    double pi = 3.14;
+
+    public double calcularArea(double radio)
+    {
+        //Console.WriteLine("El área del ciruclo es: ");
+        return  pi * radio * radio;
+    }
+}
+
+
+
+
