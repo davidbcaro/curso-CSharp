407e15147466fb5ca4ae4bde6f5318cee50375c0
David Bohorquez <davidbcaro@gmail.com>
2022-05-10 08:30:30 -0500

    Add character pattern

---
+++ 03-patron_asteriscos/03-patron_asteriscos/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ingrese el caracter para imprimir el patrón: ");
+string patron = Console.ReadLine();
+Console.WriteLine("");
+
+for (int a = 1; a <= 5; a++)
+{
+    for (int b = 1; b <= a; b++)
+    {
+        Console.Write(patron);
+    }
+    Console.WriteLine("");
+}
+for (int a = 4; a >= 1; a--)
+{
+    for (int b = 1; b <= a; b++)
+    {
+        Console.Write(patron);
+    }
+    Console.WriteLine("");
+}
