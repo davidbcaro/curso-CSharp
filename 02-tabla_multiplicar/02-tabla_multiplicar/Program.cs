6a86efc8c703ed1f30203b90c0b2a39637c387b0
David Bohorquez <davidbcaro@gmail.com>
2022-05-10 05:52:08 -0500

    Add exercise multiplication table

---
+++ 02-tabla_multiplicar/02-tabla_multiplicar/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ingrese el número de la tabla de multiplicar: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= 10; row++)
+{
+    Console.WriteLine($"{number} x {row} = {number * row}");
+}
