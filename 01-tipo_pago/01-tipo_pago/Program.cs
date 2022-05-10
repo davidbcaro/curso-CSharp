14ba76e7c671a0d17827105f57c23cfdb97abdd4
David Bohorquez <davidbcaro@gmail.com>
2022-05-09 21:38:15 -0500

    Add exercise pay type

---
+++ 01-tipo_pago/01-tipo_pago/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese el valor de la cuenta: ");
+int valor = int.Parse(Console.ReadLine());
+
+if (valor < 150000)
+{
+    Console.WriteLine("El tipo de pago a usar es efectivo");
+}
+else if (valor >= 150000 && valor <= 300000)
+{
+    Console.WriteLine("El tipo de pago a usar es pago con celular");
+}
+else if (valor > 300000 && valor <= 600000)
+{
+    Console.WriteLine("El tipo de pago a usar es tarjeta débito");
+}
+else
+{
+    Console.WriteLine("El tipo de pago a usar es tarjeta crédito");
+}
