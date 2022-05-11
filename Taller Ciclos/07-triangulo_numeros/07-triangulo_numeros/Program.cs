c7b61f2f6d18a55ecf90f18445d7bb06ec2f33bc
David Bohorquez <davidbcaro@gmail.com>
2022-05-10 20:58:07 -0500

    Add triangle numbers

---
+++ Taller Ciclos/07-triangulo_numeros/07-triangulo_numeros/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Triangulo con números");
+for (int i = 0; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j);
+
+    }
+    Console.WriteLine("");
+}
