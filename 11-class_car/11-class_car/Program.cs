8fe9532c1fc5de9cd9f53f83f6ae72aaeb391b3d
David Bohorquez <davidbcaro@gmail.com>
2022-05-16 21:25:01 -0500

    Update Car class

--- 11-class_car/11-class_car/Program.cs
+++ 11-class_car/11-class_car/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Objeto Vehículo");
 Console.WriteLine($"Marca: {MiCarro.marca}");
 Console.WriteLine($"Color: {MiCarro.color}");
 Console.WriteLine($"Placa: {MiCarro.placa}");
+Console.WriteLine($"Velocidad: {MiCarro.maxVelocidad}");
 Console.WriteLine($"Encendido: {MiCarro.encender()}");
 Console.WriteLine($"Apagado: {MiCarro.apagar()}");
 
@@ -11,7 +12,7 @@ class Car
 {
     public string marca = "Audi";
     public string color = "Blanco";
-    int maxVelocidad = 200;
+    public int maxVelocidad = 200;
     public string placa = "MTR340";
     int modelo = 2022;
     int cilindraje = 1800;
